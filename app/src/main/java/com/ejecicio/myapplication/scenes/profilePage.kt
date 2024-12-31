08f66e92919e1ea58ca3b7322a5cd652367f58fd
AlbaCastilla <castillamacki@gmail.com>
2024-12-31 14:22:20 +0100

    Btn color specification
    I didn't like the way it looked in light mode so i changed it from primary to terciary

--- app/src/main/java/com/ejecicio/myapplication/scenes/profilePage.kt
+++ app/src/main/java/com/ejecicio/myapplication/scenes/profilePage.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -44,6 +45,10 @@ fun ProfilePage(navController: NavHostController) {
                     onClick = {
                         isDarkMode = !isDarkMode // Cambiar el estado del modo oscuro al hacer clic
                     },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.surface, // Fondo del botón
+                        contentColor = MaterialTheme.colorScheme.onSurface // Color del texto
+                    ),
                     modifier = Modifier
                         .align(Alignment.CenterHorizontally) // Centra el botón
                         .padding(16.dp)
